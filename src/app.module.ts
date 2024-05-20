40867f4c53cf933de30176477dffb6365efbce91
Tetro <itsofirk@gmail.com>
2024-05-20 21:36:04 +0300

    [ app.module.ts ] Added the postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AccountModule } from './account/account.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    ConfigModule.forRoot(), // For environment variables
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 5432),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true, // Only for development, disable in production
+      ssl: false
+    }),
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
